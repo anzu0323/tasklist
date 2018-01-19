1a9e11fffd531539eecb4164293397732bd2b3ba
EC2 Default User <ec2-user@ip-172-31-16-95.us-east-2.compute.internal>
2018-01-19 06:47:06 +0000

    tasklist

---
+++ app/models/tasklist_kadai.rb
@@ -0,0 +1,2 @@
+class TasklistKadai < ApplicationRecord
+end
---
+++ test/models/tasklist_kadai_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TasklistKadaiTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
